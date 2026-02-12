ef92b7e7bf0d400f0649b1908f38d79beec102e9
Klis18 <klisnp18@gmail.com>
2026-02-12 12:28:07 -0500

    actualizando jobTitles tests

--- cypress/e2e/Admin/Jobs/JobTitles.cy.js
+++ cypress/e2e/Admin/Jobs/JobTitles.cy.js
@@ -2,22 +2,30 @@ import { jobTitles } from "../../../pages/Admin/Jobs/JobTitles";
 
 describe('Pruebas en Submódulo Jobs opción Jobs Titles', () => {
 
-    beforeEach(() =>{
+    beforeEach(function() {
+        cy.fixture('jobTitle').as('jobTitleData');
         cy.login();
         cy.redirectTo('admin/viewJobTitleList');
     });
 
-    it('Agregar Título de Trabajo', () => {
-        cy.fixture('jobTitle').then((jobTitleData) => {
-            jobTitles.addJobTitle(jobTitleData);
-        })
+    it('Agregar Título de Trabajo', function() {
+        jobTitles.add();
+        cy.wait(200);
+        jobTitles.jobTitleForm(this.jobTitleData);
+        jobTitles.submit();
     });
 
-    // it('Editar Título de Trabajo', () => {
+    
+    it('Editar Título de Trabajo', function() {
+        jobTitles.edit();
+        cy.wait(200);
+        jobTitles.jobTitleForm(this.jobTitleData);
+        jobTitles.submit();
+    });
 
-    // });
-
-    // it('Eliminar Título de Trabajo', () => {
-
-    // });
+    it('Eliminar Título de Trabajo', function(){
+        jobTitles.delete();
+        cy.wait(2000);
+        jobTitles.confirmDelete();
+    });
 })
--- cypress/pages/Admin/Jobs/JobTitles.js
+++ cypress/pages/Admin/Jobs/JobTitles.js
@@ -1,16 +1,47 @@
 
 class JobTitles{
 
-    addJobTitle(jobTitleData){
-        cy.get('button').contains('Add').click();
-        cy.wait(2000);
-        cy.get(':nth-child(2) > .oxd-input').type(jobTitleData.jobTitle);
-        cy.get('textarea[placeholder="Type description here"]').type(jobTitleData.jobDescription);
-        cy.fixture(jobTitleData.jobSpecification, null).as('myFixture');
-        cy.get('.oxd-file-input').selectFile('@myFixture',{force : true});
-        cy.get('textarea[placeholder="Add note"]').type(jobTitleData.note);
-        cy.get('button[type="submit"]').contains('Save').click();
+    jobTitleElements ={
+        addButton: () => cy.get('button').contains('Add').click(),
+        editButton: () => cy.get('button>i.oxd-icon.bi-pencil-fill'),
+        deleteButton: () => cy.get('button>i.oxd-icon.bi-trash'),
+        confirmDeleteButton: () => cy.get('button').contains('Yes, Delete').click(),
+        jobTitleInput: () => cy.get(':nth-child(2) > .oxd-input'),
+        jobTitleDescription: () => cy.get('textarea[placeholder="Type description here"]'),
+        jobInputFile: () => cy.get('.oxd-file-input').selectFile('@myFixture',{force : true}),
+        jobTitleNote: () => cy.get('textarea[placeholder="Add note"]'),
+        saveButton: () => cy.get('button[type="submit"]').contains('Save').click()
     }
+
+    add(){
+        this.jobTitleElements.addButton();
+    }
+
+    edit(){
+        this.jobTitleElements.editButton().first().click();
+    }
+
+    delete(){
+        this.jobTitleElements.deleteButton().first().click();
+    }
+
+    confirmDelete(){
+        this.jobTitleElements.confirmDeleteButton();
+    }
+
+    jobTitleForm(jobTitleData){
+        this.jobTitleElements.jobTitleInput().clear().type(jobTitleData.jobTitle);
+        this.jobTitleElements.jobTitleDescription().clear().type(jobTitleData.jobDescription);
+        cy.fixture(jobTitleData.jobSpecification, null).as('myFixture');
+        this.jobTitleElements.jobInputFile();
+        this.jobTitleElements.jobTitleNote().clear().type(jobTitleData.note);
+        this.jobTitleElements.saveButton();
+    }
+
+    submit(){
+        this.jobTitleElements.saveButton();
+    }
+
 
 }
 
